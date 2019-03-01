8d7deb2b70a5c1adf6480c41c587403aa6671b52
Cesar Guzman <Cesar.Guzman61@myhunter.cuny.edu>
2019-03-01 11:38:26 -0500

    live map-smaller

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -37,28 +37,7 @@ export default class HomeScreen extends React.Component {
     );
   }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
+ 
 
   _handleLearnMorePress = () => {
     WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
@@ -98,6 +77,8 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
+    height: 400,
+    width: 400,
   },
   contentContainer: {
     paddingTop: 30,
