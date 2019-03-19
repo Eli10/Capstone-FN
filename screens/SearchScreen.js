fd5f00c7689fc9690e93f37ff1f95f2d8af05aa1
Jasmine Wong <jasminewong@Jasmines-MacBook-Pro.local>
2019-03-19 14:57:55 -0400

    added modal function

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
-import { View, Text, FlatList, ActivityIndicator, StyleSheet, TouchableOpacity} from 'react-native';
+import { View, Text, FlatList, ActivityIndicator, StyleSheet, TouchableOpacity, Modal, Button} from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import restaurantList from '../assets/files/search.json';
 
@@ -14,7 +14,8 @@ export default class SearchScreen extends React.Component {
             loading: false,
             data: restaurantList,
             error: null,
-            value: ''
+            value: '',
+            ModalVisible: false
         };
     }
 
@@ -57,13 +58,17 @@ export default class SearchScreen extends React.Component {
         );
     };
 
-    onPress = (item) => {
-      this.props.navigation.navigate('Details', {
-        name: item.name,
-        address: item.address,
-        tags: item.type
-      });
-    };
+    // onPress = (item) => {
+    //   this.props.navigation.navigate('Details', {
+    //     name: item.name,
+    //     address: item.address,
+    //     tags: item.type
+    //   });
+    // };
+
+    showModal(visible) {
+      this.setState({ModalVisible: visible});
+    }
 
     render() {
         if (this.state.loading) {
@@ -75,12 +80,43 @@ export default class SearchScreen extends React.Component {
           }
           return (
             <View>
+              <Modal
+          transparent={false}
+ 
+          animationType={"slide"}
+ 
+          visible={this.state.ModalVisible}
+ 
+          onRequestClose={ () => { this.ShowModalFunction(!this.state.ModalVisible)} } >
+ 
+ 
+            <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
+ 
+ 
+                <View>
+ 
+ 
+                    {/* Put All Your Components Here, Which You Want To Show Inside The Modal. */}
+ 
+                    <Text>Text Component With Some Sample Text In Modal. </Text>
+ 
+                    <Button  title="Click Here To Hide Modal" onPress={() => { this.showModal(!this.state.ModalVisible)} } />
+ 
+                    {/* Put All Your Components Here, Which You Want To Show Inside The Modal. */}
+ 
+ 
+                </View>
+ 
+            </View>
+ 
+ 
+        </Modal>
               <FlatList
                 extraData={this.state}
                 data={this.state.data}
                 renderItem={({ item }) => 
 
-                <TouchableOpacity onPress={this.onPress(item)}>
+                <TouchableOpacity onPress={() => { this.showModal(true) }}>
                   <View>
                     <Text style={styles.restaurant_name}>{item.name}</Text>
                     <Text style={styles.resaturant_address}> Address: {item.address}</Text>
@@ -97,24 +133,23 @@ export default class SearchScreen extends React.Component {
     }
 }
 
-class DetailsScreen extends React.Component {
-  render() {
-    const { navigation } = this.props;
-    const name = navigation.getParam('name', 'NO-ID');
-    const address = navigation.getParam('address', 'no title');
-    const tags = navigation.getParam('tags', 'no tag');
+// class DetailsScreen extends React.Component {
+//   render() {
+//     const { navigation } = this.props;
+//     const name = navigation.getParam('name', 'NO-ID');
+//     const address = navigation.getParam('address', 'no title');
+//     const tags = navigation.getParam('tags', 'no tag');
 
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>Name: {JSON.stringify(name)}</Text>
-        <Text>Address: {JSON.stringify(address)}</Text>
-        <Text>Tags: {JSON.stringify(tags)}</Text>
-      </View>
-    );
-  }
-}
-
+//     return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Details Screen</Text>
+//         <Text>Name: {JSON.stringify(name)}</Text>
+//         <Text>Address: {JSON.stringify(address)}</Text>
+//         <Text>Tags: {JSON.stringify(tags)}</Text>
+//       </View>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   restaurant_name: {
