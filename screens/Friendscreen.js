ec52423dbb3a343cacc346286d2dced084383590
Cesar Guzman <Cesar.Guzman61@myhunter.cuny.edu>
2019-03-25 12:41:41 -0400

    for testing, two front pages updated

--- screens/Friendscreen.js
+++ screens/Friendscreen.js
@@ -11,11 +11,11 @@ import {
 import { WebBrowser } from 'expo';
 import ModalDropdown from 'react-native-modal-dropdown';
 import { MonoText } from '../components/StyledText';
+import MapView from 'react-native-maps'
 
 export default class FriendScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+  static navigationOptions = {title: 'Friends Maps',};
+
 
   render() {
     return (
@@ -28,44 +28,22 @@ export default class FriendScreen extends React.Component {
             
         />
 
-     
+        <MapView
+           style={styles.map}
+          initialRegion={{
+               latitude: 40.7128,
+              longitude: -74.0060,
+              latitudeDelta: 0.055,
+              longitudeDelta: 0.055,}}
+       />
        
       </View>
     );
   }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
 
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
@@ -73,71 +51,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
 
- 
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
      MD : {
-    paddingBottom: 100,
+    paddingBottom: 30,
     alignItems: 'center',
     flexDirection: 'column',
     justifyContent: 'space-between',
-    paddingTop: 100,
+    paddingTop: 30,
 
-  }
+  },
+  map: {
+    position: 'relative',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 485,
+    width: 400,
+    paddingTop: 100,
+  },
 });
