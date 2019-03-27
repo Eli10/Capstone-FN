50a8dfc84e3645758f7b53ceec979897beeb3822
Cesar Guzman <Cesar.Guzman61@myhunter.cuny.edu>
2019-03-27 14:39:28 -0400

    cesar commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,7 @@ import MapView from 'react-native-maps'
 import {Header,createStackNavigator, createAppContainer} from 'react-navigation'
 import ModalDropdown from 'react-native-modal-dropdown';
 
+//you are bob for tesgting
 
 import {
   Picker,
@@ -11,7 +12,7 @@ import {
   Platform,
   ScrollView,
   StyleSheet,
-  Text, 
+  Text,
   TouchableOpacity,
   View,
   AppRegistry,
@@ -28,7 +29,7 @@ import menu from '../components/DropDownClassForHomeScreen.js';
     var listnames = [];
     var restList = [];
 
-        
+
     for(var i = 0; i < RestaurantList.length; i++)
         {
             listnames.push(RestaurantList[i].listname);
@@ -38,48 +39,50 @@ import menu from '../components/DropDownClassForHomeScreen.js';
 
 console.disableYellowBox = true;
 
-var myLists = ['bubble tea', 'pizza', 'burgers', 'pizza'];
-var myLists2 = ['bubble tea', 'pizza', 'burgers', 'burgers2'];
-var myLists3 = ['bubble tea', 'pizza', 'burgers', 'food'];
+
 //refer to https://github.com/sohobloo/react-native-modal-dropdown/issues/198 to keep building functionality
 
 
 
 export default class HomeScreen extends React.Component {
-    static navigationOptions = {title: 'My Maps',}; 
-    
+    static navigationOptions = {title: 'My Maps',};
+
     constructor(props) {
     super(props);
 
-    this.state = 
-    { 
+    this.state =
+    {
         markers : restList
     }
   };
 
+    printState = () =>
+    {
+        console.log(this.state.markers)
+    }
 
   render() {
-    
 
 
-  
+
+
 
      return (
-         
+
        <View style={styles.container}>
-       
-       <ModalDropdown  
+
+       <ModalDropdown
             defaultValue = 'Please select a Map'
-            style = {styles.MD} 
+            style = {styles.MD}
             options = {listnames}
             dropdownStyle = {{ height: 35 * listnames.length}}
             onSelect={(index, value) => {this.setState({markers : popList(index)})}}
         />
-      
-   
-         
-        <MapView 
-            style={styles.map} 
+
+      {this.printState()}
+
+        <MapView
+            style={styles.map}
             initialRegion={{
               latitude: 40.7128,
               longitude: -74.0060,
@@ -88,42 +91,46 @@ export default class HomeScreen extends React.Component {
           }}
         >
 
-        
+
         {this.state.markers.map(shop => (
         <MapView.Marker
-            coordinate={{latitude: shop.latitude,
-            longitude: shop.longitude}}
-            title={shop.name}
+            coordinate={{latitude: shop.lat,
+            longitude: shop.lon}}
+            title={shop.restaurant_name}
             description={shop.address}
          /> ))}
-      </MapView> 
+      </MapView>
 
-         
+
  </View>
 
- 
+
     );
   }
 
- 
+
 
 
 }
 
       popList = (index) => {
-        var tList = [];
-        for(var i = 0; i < RestaurantList[index].list.length; i++)
-        {
+        tList = [];
+        console.log('HEILLO');
+        fetch ('http://10.0.2.2:8000/maps/Bob')
+        .then((response) => response.json())
+        .then((resData) => {
+        //console.log(resData.maps[0].restaurants);
+        return resData.maps[0].restaurants;
+        })
 
-            tList.push(RestaurantList[index].list[i]);
+
+        .catch((error) => console.log(error))
 
 
+        return  tList;
+    }
 
-        }
 
-        //console.log(tList);
-        return tList;
-      }
 
     const styles = StyleSheet.create({
         container: {
